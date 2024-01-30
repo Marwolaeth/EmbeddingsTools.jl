d29fc0db761945de7a91d86b60cf2b3cf05f4b9e
Culturology's Undead <a.pawluczenko@gmail.com>
2024-01-30 15:56:56 +0300

    Tests for embedding reduction
    Added tests for dimensionality reduction function

--- test/runtests.jl
+++ test/runtests.jl
@@ -44,7 +44,7 @@ using JET
                 emb_ind = index(emb)
                 @test try
                     # This will throw an error in any case:
-                    EmbeddingsTools.get_vector(index(read_vec("tiny.vec")), "Sinister")
+                    get_vector(index(read_vec("tiny.vec")), "Sinister")
                 catch e
                     # This is what we are interested in: is the exception right?
                     isa(e, EmbeddingsTools.TokenNotFoundException)
@@ -67,6 +67,26 @@ using JET
                 @test subspace(emb_ind, tokens).vocab[2] ≡ "to"
                 @test subspace(emb_ind, tokens).ndims ≡ emb.ndims
                 @test subspace(emb_ind, tokens).ntokens ≡ length(tokens)
+            end
+            # Dimensionality Reduction
+            @testset "Embedding Dimensionality Reduction" begin
+                emb = read_vec("tiny.vec")
+                emb_ind = index(emb)
+                @test isa(reduce_emb(emb, 2), WordEmbedding)
+                @test isa(reduce_emb(emb, 2, method="SVD"), WordEmbedding)
+                @test isa(reduce_emb(emb_ind, 2), IndexedWordEmbedding)
+                @test isa(reduce_emb(emb_ind, 2, method="SVD"), IndexedWordEmbedding)
+                @test try
+                    # This should throw an error:
+                    reduce_emb(emb, 2, method="ppca")
+                catch e
+                    # A specific kind of error
+                    isa(e, EmbeddingsTools.UnknownReductionMethodException)
+                end
+                @test size(reduce_emb(emb, 2).embeddings) ≡ (2, 4)
+                @test size(reduce_emb(emb, 13).embeddings) ≡ (4, 4)
+                @test reduce_emb(emb_ind, 1).ndims ≡ 1
+                @test reduce_emb(emb_ind, 13, method="svd").ndims ≡ 4
             end
         end
     end
